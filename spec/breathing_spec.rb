34e017f1477de8b8ff448bf39041ead7a42e56cd
Akira Kusumoto <akirakusumo10@gmail.com>
2020-12-10 20:45:21 +0900

    :wrench: Setup CircleCI

--- spec/breathing_spec.rb
+++ spec/breathing_spec.rb
@@ -4,4 +4,9 @@ describe Breathing do
   it 'has a version number' do
     expect(Breathing::VERSION).not_to be nil
   end
+
+  it do
+    user = User.create!(name: :a, age: 1)
+    expect(user.name).to eq('a')
+  end
 end
