8254903ccd8696ea0a0e71edc04aa9e026fb787f
Akira Kusumoto <akirakusumo10@gmail.com>
2020-12-14 18:38:36 +0900

    :recycle: use hairtrigger gem

--- lib/breathing/trigger.rb
+++ lib/breathing/trigger.rb
@@ -12,59 +12,48 @@ module Breathing
 
     def create
       trigger_name = "#{log_table_name}_insert_#{model.table_name}"
+
       unless exists?(trigger_name)
         puts "CREATE TRIGGER #{trigger_name}"
-        ActiveRecord::Base.connection.execute <<-SQL
-        CREATE TRIGGER #{trigger_name}
-        AFTER INSERT
-        ON #{model.table_name}
-        FOR EACH ROW
-        BEGIN
+
+        ActiveRecord::Base.connection.create_trigger(trigger_name).on(model.table_name).after(:insert) do
+          <<-SQL
           INSERT INTO #{log_table_name} (`action`, `table_name`, `transaction_id`, `before_data`, `after_data`, `created_at`)
           VALUES ('INSERT', '#{model.table_name}', NEW.id,
                   JSON_OBJECT(),
                   JSON_OBJECT(#{json_object_values(model.columns, 'NEW')}),
                   CURRENT_TIMESTAMP);
-        END;
-        SQL
+          SQL
+        end
       end
 
       trigger_name = "#{log_table_name}_update_#{model.table_name}"
       unless exists?(trigger_name)
         puts "CREATE TRIGGER #{trigger_name}"
-        ActiveRecord::Base.connection.execute <<-SQL
-        CREATE TRIGGER #{trigger_name}
-        BEFORE UPDATE
-        ON #{model.table_name}
-        FOR EACH ROW
-        BEGIN
-          IF (OLD.updated_at != NEW.updated_at) THEN
-              INSERT INTO #{log_table_name} (`action`, `table_name`, `transaction_id`, `before_data`, `after_data`, `created_at`)
+
+        ActiveRecord::Base.connection.create_trigger(trigger_name).on(model.table_name).before(:update).of(:updated_at) do
+          <<-SQL
+          INSERT INTO #{log_table_name} (`action`, `table_name`, `transaction_id`, `before_data`, `after_data`, `created_at`)
               VALUES ('UPDATE', '#{model.table_name}', NEW.id,
                       JSON_OBJECT(#{json_object_values(model.columns, 'OLD')}),
                       JSON_OBJECT(#{json_object_values(model.columns, 'NEW')}),
                       CURRENT_TIMESTAMP);
-          end if;
-        END;
-        SQL
+          SQL
+        end
       end
 
       trigger_name = "#{log_table_name}_delete_#{model.table_name}"
       unless exists?(trigger_name)
         puts "CREATE TRIGGER #{trigger_name}"
-        ActiveRecord::Base.connection.execute <<-SQL
-        CREATE TRIGGER #{trigger_name}
-        AFTER DELETE
-        ON #{model.table_name}
-        FOR EACH ROW
-        BEGIN
-          INSERT INTO #{log_table_name} (`action`, `table_name`, `transaction_id`, `before_data`, `after_data`, `created_at`)
-          VALUES ('DELETE', '#{model.table_name}', OLD.id,
+        ActiveRecord::Base.connection.create_trigger(trigger_name).on(model.table_name).after(:delete) do
+          <<-SQL
+           INSERT INTO #{log_table_name} (`action`, `table_name`, `transaction_id`, `before_data`, `after_data`, `created_at`)
+           VALUES ('DELETE', '#{model.table_name}', OLD.id,
                   JSON_OBJECT(#{json_object_values(model.columns, 'OLD')}),
                   JSON_OBJECT(),
                   CURRENT_TIMESTAMP);
-        END;
-        SQL
+          SQL
+        end
       end
     end
 
@@ -86,8 +75,7 @@ module Breathing
     private
 
     def exists?(trigger_name)
-      @trigger_names ||= ActiveRecord::Base.connection.select_rows('SHOW TRIGGERS').map { |row| row.first }
-      @trigger_names.include?(trigger_name)
+      ActiveRecord::Base.connection.triggers.keys.include?(trigger_name)
     end
 
     def json_object_values(columns, state)
